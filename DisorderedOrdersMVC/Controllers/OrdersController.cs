b9ea13a61b931174057b5c839ac1e802ae058a1f
jkimball54 <jeremyck54@gmail.com>
2023-10-16 15:33:48 -0600

    moved static methods in OrdersControllers -> Order Model

--- DisorderedOrdersMVC/Controllers/OrdersController.cs
+++ DisorderedOrdersMVC/Controllers/OrdersController.cs
@@ -28,13 +28,13 @@ namespace DisorderedOrdersMVC.Controllers
         public IActionResult Create(IFormCollection collection, string paymentType)
         {
             // create order
-            var order = createOrder(collection, _context);
+            var order = new Order(collection, _context);
             // verify stock available
-            verifyStock(order);
+            order.verifyStock();
             // calculate total price
-            var total = calculateTotalPrice(order);
+            var total = order.calculateTotalPrice();
             // process payment
-            processPayment(_context, order, total, paymentType);
+            order.processPayment(_context, total, paymentType);
 
 			return RedirectToAction("Show", new { id = order.Id });
 		}
@@ -58,70 +58,5 @@ namespace DisorderedOrdersMVC.Controllers
 
             return View(order);
         }
-
-        //create order
-        static Order createOrder(IFormCollection collection, DisorderedOrdersContext _context)
-        {
-			int customerId = Convert.ToInt16(collection["CustomerId"]);
-			Customer customer = _context.Customers.Find(customerId);
-			var order = new Order() { Customer = customer };
-			for (var i = 1; i < collection.Count - 1; i++)
-			{
-				var kvp = collection.ToList()[i];
-				if (kvp.Value != "0")
-				{
-					var product = _context.Products.Where(p => p.Name == kvp.Key).First();
-					var orderItem = new OrderItem() { Item = product, Quantity = Convert.ToInt32(kvp.Value) };
-					order.Items.Add(orderItem);
-				}
-			}
-            return order;
-		}
-        //verify stock available
-        static void verifyStock(Order order)
-        {
-			foreach (var orderItem in order.Items)
-			{
-				if (!orderItem.Item.InStock(orderItem.Quantity))
-				{
-					orderItem.Quantity = orderItem.Item.StockQuantity;
-				}
-
-				orderItem.Item.DecreaseStock(orderItem.Quantity);
-			}
-		}
-        //calculate total price
-        static int calculateTotalPrice(Order order)
-        {
-			var total = 0;
-			foreach (var orderItem in order.Items)
-			{
-				var itemPrice = orderItem.Item.Price * orderItem.Quantity;
-				total += itemPrice;
-			}
-            return total;
-		}
-		//process payment
-		static void processPayment(DisorderedOrdersContext _context, Order order, int total, string paymentType)
-		{
-            IPaymentProcessor processor;
-            if (paymentType == "bitcoin")
-            {
-                processor = new BitcoinProcessor();
-            }
-            else if (paymentType == "paypal")
-            {
-                processor = new PayPalProcessor();
-            }
-            else
-            {
-                processor = new CreditCardProcessor();
-            }
-
-            processor.ProcessPayment(total);
-
-            _context.Orders.Add(order);
-            _context.SaveChanges();
-		}
 	}
 }
